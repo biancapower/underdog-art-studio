e96630514fc0cbec4e4c15807abe055f725c0e73
Bianca Power <drbiancampower@gmail.com>
2017-11-17 13:19:44 +1100

    users must be signed up to access admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-      devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
-    # end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
 
   root 'static_pages#homepage'
 end
