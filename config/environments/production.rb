204ee8a44ddf20969786f27e228428009912353f
Bianca Power <drbiancampower@gmail.com>
2017-11-19 12:51:51 +1100

    added amazon s3 config details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV['AWS_REGION'],
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      s3_host_name: ENV['AWS_S3_HOST_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
