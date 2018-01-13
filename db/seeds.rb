ec6dc42694f110bcf19607d1eb24b09efd678133
Bianca Power <drbiancampower@gmail.com>
2018-01-13 16:40:33 +1100

    seed user admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!({email: 'biancampower@gmail.com', superadmin_role: true, password: 'casscass', password_confirmation: 'casscass'})
