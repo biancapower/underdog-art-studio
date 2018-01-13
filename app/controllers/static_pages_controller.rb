b48fd54a21ec2c068cef2759cea50aceef0711e5
Bianca Power <drbiancampower@gmail.com>
2018-01-13 15:50:56 +1100

    correct items display on product pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,15 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
+
+  def prints
+    @items = Item.joins(:product_types).where('product_types.name' => ProductType.Prints).distinct
+  end
+
+  def stickers
+    @items = ProductType.find_by( name: 'Sticker') .items
+  end
+
+  def shirts
+    @items = ProductType.find_by( name: 'Shirt') .items
+  end
 end
--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,3 +1,14 @@
 class ProductType < ApplicationRecord
   has_and_belongs_to_many :items
+
+  @PrintsA4 = 'Print - A4'
+  @PrintsA3 = 'Print - A3'
+  @PrintsA5 = 'Print - A5'
+  
+  @Prints = ['Print - A4', 'Print - A3', 'Print - A5']
+
+  class << self
+    attr_reader :PrintsA4, :PrintsA3, :PrintsA5, :Prints
+  end
+
 end
